dabf147de14676ad8c4ff7aec225269a3cdbfc32
Pablo <pablo.estrada@smartcontract.com>
2025-07-09 08:20:38 -0600

    fix: remove network id flag

--- tests/containers/geth.go
+++ tests/containers/geth.go
@@ -44,7 +44,6 @@ func NewGethContainer(ctx context.Context) (*GethContainer, error) {
 			"--http", "--http.addr", "0.0.0.0", "--http.vhosts", "*",
 			"--ws", "--ws.addr", "0.0.0.0", "--ws.origins", "*",
 			"--cache.blocklogs", "1024",
-			"--networkid", fmt.Sprintf("%d", chainID),
 			"--datadir", dataDir,
 		},
 		// uncomment to print container logs to stdout
