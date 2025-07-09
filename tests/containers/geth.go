03046e91b1ca42c4f359aa52437d095a3c1ba9e3
Pablo <pablo.estrada@smartcontract.com>
2025-07-09 08:04:49 -0600

    fix: debug geth container startup

--- tests/containers/geth.go
+++ tests/containers/geth.go
@@ -48,12 +48,12 @@ func NewGethContainer(ctx context.Context) (*GethContainer, error) {
 			"--datadir", dataDir,
 		},
 		// uncomment to print container logs to stdout
-		// LogConsumerCfg: &testcontainers.LogConsumerConfig{
-		// 	Opts: []testcontainers.LogProductionOption{
-		// 		testcontainers.WithLogProductionTimeout(10 * time.Second),
-		// 	},
-		// 	Consumers: []testcontainers.LogConsumer{&StdoutLogConsumer{Prefix: "|| "}},
-		// },
+		LogConsumerCfg: &testcontainers.LogConsumerConfig{
+			Opts: []testcontainers.LogProductionOption{
+				testcontainers.WithLogProductionTimeout(10 * time.Second),
+			},
+			Consumers: []testcontainers.LogConsumer{&StdoutLogConsumer{Prefix: "|| "}},
+		},
 	}
 	gethContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: request,
